49f9c5dc36266e2b7780372bc02a348b54ab0301
RodriguesEric134 <rm550249@fiap.com.br>
2025-02-10 14:26:28 -0300

    alterações no arquivo de avaliação

---
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    public Avaliacao(int nota) 
+    {
+        Nota = nota;
+    }
+
+    public int Nota { get; }
+
+    public static Avaliacao Parse(String texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+
+    }
+
+}
